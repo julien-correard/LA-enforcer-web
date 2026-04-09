e520cb5004a95b430c41095a9345a8028b3426a9
Gé <zoso85@hotmail.fr>
2026-04-09 21:02:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# LA Enforcer – Web site
+
+TODO
+
+## Auteur
+
+Projet réalisé par Julien CORREARD.
+
+## Licence
+
+Ce projet est sous licence MIT. Voir fichier LICENSE.
---
+++ script.js
@@ -0,0 +1,23 @@
+fetch("http://localhost:8080/scores")
+  .then(res => res.json())
+  .then(data => {
+
+    let text = "";
+
+    data.forEach((s, i) => {
+
+      const rank = String(i + 1).padEnd(3, " ");
+      const name = String(s.playerName).padEnd(15, " ");
+      const score = String(s.score).padStart(6, " ");
+      const date = String(s.date).padEnd(12, " ");
+      const version = `game version : ${s.gameVersion}`;
+
+      text += `${rank}${name}${score}  ${date}  ${version}\n`;
+    });
+
+    document.getElementById("output").textContent = text;
+  })
+  .catch(err => {
+    document.getElementById("output").textContent = "Erreur connexion serveur";
+    console.error(err);
+  });
